846f9961b70ee4605185b94c05e8ae8ba68e05ef
pardiproop <prooprai1@myseneca.ca>
2026-09-25 16:53:42 +0000

    Completed Lab2e in Class

--- lab2a.py
+++ lab2a.py
@@ -1,8 +1,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Pardip Rooprai
+# Date: 25/9/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
--- lab2d.py
+++ lab2d.py
@@ -1,8 +1,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date: Learn how to use command line arguments.
+# Author: Pardip Rooprai
+# Date: 25/9/2026
 # Purpose: .
 # Usage: ./lab2d.py
 
@@ -12,4 +12,18 @@
 
 # TO DO 2: copy the required lines from README.md to print argv[0], argv[1] and argv[2]
 # run the script using the following command: python lab2d.py maija Maija
+import sys
+print(sys.version) # prints the version of the python currently in use.
+print(sys.platform) # prints the name of the operating system.
+print(sys.argv) # prints the list of all arguments given at the command line when running our py
+print(len(sys.argv)) # prints the number of all arguments
 
+print(sys.version) # prints the version of the python currently in use.
+print(sys.platform) # prints the name of operating system.
+print(sys.argv) # prints the list of all arguments given at the command line when running our python script from terminal.
+print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.
+
+print(sys.argv[0]) # prints the first argument, it is always the name of script.
+print(sys.argv[1]) # prints the second argument .
+print(sys.argv[2]) # prints the third argument.
+print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.
